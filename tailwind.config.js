40cead6e12a6008d995a01c2a665cf53177f9ba5
youngwiez <m.raka248@gmail.com>
2023-11-29 20:42:39 +0700

    test

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily : {
+        "raleway" : ['raleway']
+      },
+      colors : {
+        "abu" : '#909090'
+      }
+    },
+  },
+  plugins: [],
+}
